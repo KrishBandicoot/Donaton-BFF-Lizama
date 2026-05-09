de73fa5df9a7cebf88ce9334eb73b23a9a6621f2
KrishBandicoot <cristian.lizama.diaz2014@gmail.com>
2026-05-09 01:29:54 -0400

    Agregando Zonas de acopio, destino y tipos de ayuda

--- src/main/java/com/donaton/bff/controller/BffController.java
+++ src/main/java/com/donaton/bff/controller/BffController.java
@@ -7,8 +7,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -28,5 +29,15 @@ public class BffController {
         resumen.put("logistica", integracionService.obtenerEnviosLogistica());
         
         return ResponseEntity.ok(resumen);
+    }
+    @GetMapping("/opciones")
+    public ResponseEntity<Map<String, List<String>>> obtenerOpcionesFormulario() {
+        Map<String, List<String>> opciones = new HashMap<>();
+        
+        opciones.put("tiposAyuda", Arrays.asList("Alimento", "Ropa de Abrigo", "Insumos Médicos", "Insumos de Higiene", "Materiales de Construcción"));
+        opciones.put("centrosAcopio", Arrays.asList("Centro Central Santiago", "Sede Valparaíso", "Gimnasio Concepción", "Municipalidad de Maipú"));
+        opciones.put("destinos", Arrays.asList("Refugio Maipú", "Campamento Viña del Mar", "Zona Cero Sur", "Albergue Estadio Nacional"));
+        
+        return ResponseEntity.ok(opciones);
     }
 }
